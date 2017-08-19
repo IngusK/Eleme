ef8247890a7193d69f4d3835025fdc99f84d437c
Ingus Kruklitis <Ingus@Inguss-iMac.local>
2017-08-20 00:53:41 +0300

    improved products page

--- src/app/routes/contacts/index.jsx
+++ src/app/routes/contacts/index.jsx
@@ -16,20 +16,22 @@ export default class Contacts extends React.PureComponent {
   render() {
     return (
       <div className="contacts-content">
-        <h1>Contact Us</h1>
-        <h3>Domina shopping mall Ieriķu street 3, Vidzemes priekšpilsēta, Riga, LV-1084</h3>
-        <h3>Our phone number: +371 2 678 5698, +371 6 236 2554</h3>
-        <div className="map-block" onClick={this.scrollOn} onMouseLeave={this.scrollOff}>
-          <iframe id="iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2174.977728676661!2d24.15891831661045!3d56.966313980895265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eece4f87a96f9f%3A0xd4b908bee3cb6b97!2sDomina+Shopping!5e0!3m2!1sen!2slv!4v1501952841161" width="100%" height="500" frameborder="0" allowfullscreen></iframe>
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
+        <div className="contacts-grid">
+          <h1>Contact Us</h1>
+          <h3>Domina shopping mall Ieriķu street 3, Vidzemes priekšpilsēta, Riga, LV-1084</h3>
+          <h3>Our phone number: +371 2 678 5698, +371 6 236 2554</h3>
+          <div className="map-block" onClick={this.scrollOn} onMouseLeave={this.scrollOff}>
+            <iframe id="iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2174.977728676661!2d24.15891831661045!3d56.966313980895265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eece4f87a96f9f%3A0xd4b908bee3cb6b97!2sDomina+Shopping!5e0!3m2!1sen!2slv!4v1501952841161" width="100%" height="500" frameborder="0" allowfullscreen></iframe>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+          <h2>Eleme Smart Home</h2>
+          <p>Sengled GmbH is an international manufacturer of innovative and smart lighting products. We have more than 20 years’ experience in the lighting industry. With developers and designers in Germany, the USA and China and over 200 patents, we are different to other traditional lighting product manufacturers. We aim to impress and inspire people with our products. You’ll notice the Wow! effect as soon as you try a Sengled product. Our products are always a little different, a little more modern and, above all, simple to operate and useful.</p>
+          <p>Sengled GmbH is an international manufacturer of innovative and smart lighting products. We have more than 20 years’ experience in the lighting industry. With developers and designers in Germany, the USA and China and over 200 patents.</p>
         </div>
-        <h2>Eleme Smart Home</h2>
-        <p>Sengled GmbH is an international manufacturer of innovative and smart lighting products. We have more than 20 years’ experience in the lighting industry. With developers and designers in Germany, the USA and China and over 200 patents, we are different to other traditional lighting product manufacturers. We aim to impress and inspire people with our products. You’ll notice the Wow! effect as soon as you try a Sengled product. Our products are always a little different, a little more modern and, above all, simple to operate and useful.</p>
-        <p>Sengled GmbH is an international manufacturer of innovative and smart lighting products. We have more than 20 years’ experience in the lighting industry. With developers and designers in Germany, the USA and China and over 200 patents.</p>
       </div>
     );
   }
--- src/app/routes/products/index.jsx
+++ src/app/routes/products/index.jsx
@@ -8,82 +8,134 @@ export default class Products extends React.PureComponent {
   render() {
     return (
       <div className="products-content">
-        <h2>Products</h2>
-        <p>Sengled GmbH is an international manufacturer of innovative and smart lighting products. We have more than 20 years’ experience in the lighting industry. With developers and designers in Germany, the USA and China and over 200 patents, we are different to other traditional lighting product manufacturers. We aim to impress and inspire people with our products. You’ll notice the Wow! effect as soon as you try a Sengled product. Our products are always a little different, a little more.</p>
-        <div className="slider-wrapper">
-          <ul className="slider-buttons">
-            <li>
-              <img src="../img/energy-photo.jpg" alt="Energy photo" />
-              <Link to='/specific-product'>Energy</Link>
-            </li>
-            <li>
-              <img src="../img/safety-photo.jpg" alt="Safety photo" />
-              <Link to='/specific-product'>Safety</Link></li>
-            <li>
-              <img src="../img/comfort-photo.jpg" alt="Comfort photo" />
-              <Link to='/specific-product'>Comfort</Link>
-            </li>
-          </ul>
-        </div>
-        <h2>Choose by our brands</h2>
-        <div className="brands">
-          <div className="brand-block">
-            <div>
-              <a href="#"><img src="../img/logos/netatmo-logo.png" alt="netatmo logo" /></a>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
-            </div>
-            <Link className="view-button" to='/specific-product'>View</Link>
+        <div className="contacts-grid">
+          <h2>Products</h2>
+          <p>Sengled GmbH is an international manufacturer of innovative and smart lighting products. We have more than 20 years’ experience in the lighting industry. With developers and designers in Germany, the USA and China and over 200 patents, we are different to other traditional lighting product manufacturers. We aim to impress and inspire people with our products. You’ll notice the Wow! effect as soon as you try a Sengled product. Our products are always a little different, a little more.</p>
+          <div className="slider-wrapper">
+            <ul className="slider-buttons">
+              <li>
+                <img src="../img/energy-photo.jpg" alt="Energy photo" />
+                <Link to='/product-list'>Energy</Link>
+              </li>
+              <li>
+                <img src="../img/safety-photo.jpg" alt="Safety photo" />
+                <Link to='/product-list'>Safety</Link></li>
+              <li>
+                <img src="../img/comfort-photo.jpg" alt="Comfort photo" />
+                <Link to='/product-list'>Comfort</Link>
+              </li>
+            </ul>
           </div>
-          <div className="brand-block">
-            <div>
-              <a href="#"><img src="../img/logos/senged-logo.png" alt="netatmo logo" /></a>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+          <h2>Choose by our devices</h2>
+          <div className="brands-category">
+            <div className="brand-block">
+              <div>
+                <Link to='/product-list'><img src="../img/energy-photo.jpg" alt="Brand logo" /></Link>
+                <h4>Mobile devices</h4>
+              </div>
             </div>
-            <Link className="view-button" to='/specific-product'>View</Link>
-          </div>
-          <div className="brand-block">
-            <div>
-              <a href="#"><img src="../img/logos/netatmo-logo.png" alt="netatmo logo" /></a>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+            <div className="brand-block">
+              <div>
+                <Link to='/product-list'><img src="../img/energy-photo.jpg" alt="Brand logo" /></Link>
+                <h4>Mobile devices</h4>
+              </div>
             </div>
-            <Link className="view-button" to='/specific-product'>View</Link>
-          </div>
-          <div className="brand-block">
-            <div>
-              <a href="#"><img src="../img/logos/senged-logo.png" alt="netatmo logo" /></a>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+            <div className="brand-block">
+              <div>
+                <Link to='/product-list'><img src="../img/main-photo.jpg" alt="Brand logo" /></Link>
+                <h4>Mobile devices</h4>
+              </div>
             </div>
-            <Link className="view-button" to='/specific-product'>View</Link>
-          </div>
-          <div className="brand-block">
-            <div>
-              <a href="#"><img src="../img/logos/netatmo-logo.png" alt="netatmo logo" /></a>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+            <div className="brand-block">
+              <div>
+                <Link to='/product-list'><img src="../img/energy-photo.jpg" alt="Brand logo" /></Link>
+                <h4>Mobile devices</h4>
+              </div>
             </div>
-            <Link className="view-button" to='/specific-product'>View</Link>
-          </div>
-          <div className="brand-block">
-            <div>
-              <a href="#"><img src="../img/logos/senged-logo.png" alt="netatmo logo" /></a>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+            <div className="brand-block">
+              <div>
+                <Link to='/product-list'><img src="../img/energy-photo.jpg" alt="Brand logo" /></Link>
+                <h4>Mobile devices</h4>
+              </div>
             </div>
-            <Link className="view-button" to='/specific-product'>View</Link>
-          </div>
-          <div className="brand-block">
-            <div>
-              <a href="#"><img src="../img/logos/netatmo-logo.png" alt="netatmo logo" /></a>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+            <div className="brand-block">
+              <div>
+                <Link to='/product-list'><img src="../img/energy-photo.jpg" alt="Brand logo" /></Link>
+                <h4>Mobile devices</h4>
+              </div>
             </div>
-            <Link className="view-button" to='/specific-product'>View</Link>
-          </div>
-          <div className="brand-block">
-            <div>
-              <a href="#"><img src="../img/logos/senged-logo.png" alt="netatmo logo" /></a>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+            <div className="brand-block">
+              <div>
+                <Link to='/product-list'><img src="../img/energy-photo.jpg" alt="Brand logo" /></Link>
+                <h4>Mobile devices</h4>
+              </div>
             </div>
-            <Link className="view-button" to='/specific-product'>View</Link>
+            <div className="brand-block">
+              <div>
+                <Link to='/product-list'><img src="../img/energy-photo.jpg" alt="Brand logo" /></Link>
+                <h4>Mobile devices</h4>
+              </div>
+            </div>
           </div>
-
+          <h2>Choose by our brands</h2>
+          <div className="brands">
+            <div className="brand-block">
+              <div>
+                <a href="#"><img src="../img/logos/netatmo-logo.png" alt="netatmo logo" /></a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+              <Link className="view-button" to='/product-list'>View</Link>
+            </div>
+            <div className="brand-block">
+              <div>
+                <a href="#"><img src="../img/logos/senged-logo.png" alt="netatmo logo" /></a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+              <Link className="view-button" to='/product-list'>View</Link>
+            </div>
+            <div className="brand-block">
+              <div>
+                <a href="#"><img src="../img/logos/netatmo-logo.png" alt="netatmo logo" /></a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+              <Link className="view-button" to='/product-list'>View</Link>
+            </div>
+            <div className="brand-block">
+              <div>
+                <a href="#"><img src="../img/logos/senged-logo.png" alt="netatmo logo" /></a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+              <Link className="view-button" to='/product-list'>View</Link>
+            </div>
+            <div className="brand-block">
+              <div>
+                <a href="#"><img src="../img/logos/netatmo-logo.png" alt="netatmo logo" /></a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+              <Link className="view-button" to='/product-list'>View</Link>
+            </div>
+            <div className="brand-block">
+              <div>
+                <a href="#"><img src="../img/logos/senged-logo.png" alt="netatmo logo" /></a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+              <Link className="view-button" to='/product-list'>View</Link>
+            </div>
+            <div className="brand-block">
+              <div>
+                <a href="#"><img src="../img/logos/netatmo-logo.png" alt="netatmo logo" /></a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+              <Link className="view-button" to='/product-list'>View</Link>
+            </div>
+            <div className="brand-block">
+              <div>
+                <a href="#"><img src="../img/logos/senged-logo.png" alt="netatmo logo" /></a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+              <Link className="view-button" to='/product-list'>View</Link>
+            </div>
+          </div>
         </div>
       </div>
     );
